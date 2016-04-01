1435fdf3f82dd84e519d09c37da88f93a84e22e4
Michael Bauer <mbauer14@gmail.com>
2016-04-01 14:08:27 -0500

    Update example.py

--- example/example.py
+++ example/example.py
@@ -2,6 +2,7 @@ import os
 
 import riffle
 
+logOutput = os.environ.get("LOG_HEADER", "default")
 
 class ExampleSession(riffle.Domain):
     def onJoin(self):
@@ -9,12 +10,13 @@ class ExampleSession(riffle.Domain):
 
     @riffle.want(str)
     def echo(self, msg):
-        print("Echo: {}".format(msg))
+        print("{}: {}".format(logOutput, msg))
         return msg
 
 
 if __name__ == "__main__":
     riffle.SetFabric(os.environ['WS_URL'])
+    
     riffle.SetLogLevelInfo()
     domain = os.environ['DOMAIN']
     ExampleSession(domain).join()
